1989d465661ae123248bb6bc19495241753307b7
MR.51MB4H <48198896+kingcracker@users.noreply.github.com>
2019-05-19 22:31:58 +0700

    Add files via upload

---
+++ Create.sh
@@ -0,0 +1,130 @@
+# ! /usr/bin/bash
+#tools sederhana
+#dont recode bangsat
+#by MR.51MB4H
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo $yellow
+echo " "
+cat sy.txt
+echo  $blue "<!÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷!>"
+echo  $red    "|       TOOLS Create Script Deface        |"
+echo  $red    "|                                         |"
+echo  $red    "| Author        : Mr.51mb4h               |"
+echo  $red    "| date          : 11-4-2019               |"
+echo  $red    "| My Team       : BANYUMAS CYBER TEAM     |"
+echo  $red    "| selamat mencoba njiing                  |"
+echo  $blue "<!÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷!>"
+
+sleep 5
+echo " "
+echo $green "============== $red MENU $green ================="
+sleep 2
+echo $blue "===================================="
+sleep 2
+echo $blue "|| $green 1. BUAT Script deface biasa      $blue ||"
+sleep 2
+echo $blue "|| $green 2. Buat Script deface sedang     $blue ||"
+sleep 2
+echo $blue "|| $green 3. Buat script defac             $blue ||"
+sleep 2
+echo $blue "|| $green 4. Deface poc Webdav             $blue ||"
+sleep 2
+echo $blue "|| $green 0. Exit The Program              $blue ||"
+echo $blue "===================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────fight@KING CRACKER=•>" King ;
+
+if [ $King = 1 ] || [ $King = 01 ]
+then
+clear
+figlet "BCT" | lolcat
+echo $blue "semua butuh proses"
+sleep 1
+echo $yellow "AHAHA"
+pkg install update
+pkg install git
+pkg install python2
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+sleep 1
+echo $blue  "thanks udah gunakan tools ini :V"
+sleep 2
+echo $yellow "BANYUMAS CYBER TEAM WAS HERE"
+fi
+
+
+if [ $King = 2 ] || [ $King = 02 ]
+then
+sleep 2
+clear
+figlet "BCT" | lolcat
+echo $blue "semua butuh proses...."
+sleep 3
+echo $green "Buat yang bener asu"
+pkg install python2
+python2 Script.py
+sleep 1
+echo $blue "thanks udah gunakan tools ini :V"
+sleep 2
+echo $yellow "BANYUMAS CYBER TEAM WAS HERE"
+fi
+
+if [ $King = 3 ] || [ $King = 03 ]
+then
+clear
+figlet "BCT" | lolcat
+echo $blue "semua butuh proses......"
+sleep 1
+echo $yellow "AHAHA"
+pkg install update && upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+python2 create.py
+sleep 1
+echo $blue "thanks udah gunakan tools ini :V"
+sleep 2
+echo $yellow "BANYUMAS CYBER TEAM WAS HERE"
+fi
+
+if [ $King = 4 ] || [ $King = 04 ]
+then
+clear
+figlet "BCT" | lolcat
+echo $blue "semua butuh proses"
+sleep 1
+echo $yellow "AHAHA"
+sleep 3
+echo $green "Buat yang bener asu"
+pkg install python
+python action.py
+sleep 1
+echo $blue "thanks udah gunakan tools ini :V"
+sleep 2
+echo $yellow "BANYUMAS CYBER TEAM WAS HERE"
+fi
+
+
+if [ $King = 0 ]||[ $King = 00 ]
+then
+echo $cyan
+sleep 2
+cat bla.txt
+sleep 1
+echo  $red "dah dah.."
+sleep 2
+echo  $blue "thanks sudah gunakan tools ini"
+sleep 2
+echo  $green "BOKEP DULU SANA!!!!"
+sleep 2
+echo $yellow "BANYUMAS CYBER TEAM WAS HERE..."
+sleep 1
+exit
+fi
